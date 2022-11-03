bf0cc841909974944a2b070bbfa15734b91beb93
alexbbd03 <001zzz001zzz@gmail.com>
2022-11-03 15:24:19 +0200

    feat: add starting project files

---
+++ src/pages/PortoPage/PortoPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PortoPage = () => {
+  return (
+    <div>
+      <h1>Porto Page</h1>
+    </div>
+  );
+};
+
+export default PortoPage;
---
+++ src/routes/LaunchRouter.tsx
@@ -0,0 +1,16 @@
+import LandingPage from '@pages/LandingPage/LandingPage';
+import PortoPage from '@pages/PortoPage/PortoPage';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoutes } from './AppRoutes';
+
+const LaunchRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoutes.LANDING} element={<LandingPage />} />
+        <Route path={AppRoutes.PORTOFOLIO} element={<PortoPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
